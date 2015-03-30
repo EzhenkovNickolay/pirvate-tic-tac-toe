2dc22bb006d080fcd7119b07b76f3e8d5035602d
NickolayEzhenkov <Nickolay_Ezhenkov@epam.com>
2015-03-30 16:40:12 +0300

    add link to previous page and coomend js and css

--- tic-tac/const.js
+++ tic-tac/const.js
@@ -1,5 +1,6 @@
 const initMaxStep = 25;
 const initStep = 0;
+var currentPage = 'main';
 var currentRound = 0;
 var currentStep = 0;
 var maxStep = 25;
--- tic-tac/script.js
+++ tic-tac/script.js
@@ -1,68 +1,8 @@
-
-function push(elem){
-	if(!gameover){
-		var d = document.getElementById(elem);
-		if(d.innerHTML == ''){
-			if(currentStep % 2 == 0){
-				d.innerHTML = '<div class="round">';
-			}
-			else{
-				d.innerHTML = '<div class="cross">';
-			}		
-			currentStep++;
-		}
-		if(currentStep == maxStep){
-			alert('Draw!!!');
-			gameover = true;
-			onemoreGame();
-		}
-	}
-}
-
+/*Functia najatia knopki Single game vizivaet menu vibora soperniika */
 function singleGame(){
 	var content = document.getElementById('menu-bar');
 	content.style.display = 'none';
-	var panel = document.getElementById('partner-panel');
-	panel.style.display = 'block';
-}
 
-function growUpImage(defId, imgId){	
-	var definition = document.getElementById(defId);
-	definition.style.color = 'blue'
-	definition.style.fontSize = '35';
-	
-	var image  =  document.getElementById(imgId);
-	image.style.height = '90%';
-	image.style.width = '90%';
-}
-
-function growDownImage(defId, imgId){	
-	var definition = document.getElementById(defId);
-	definition.style.color = 'white'
-	definition.style.fontSize = '30';	
-	
-	var image  =  document.getElementById(imgId);
-	image.style.height = '80%';
-	image.style.width = '80%';
-}
-
-function startWithHuman(){
-	var panel = document.getElementById('partner-panel');
-	panel.style.display = 'none';
-
-	var content = document.getElementById('tic-tac-wrap');
-	content.style.display = 'block';
-	
-	var desk = document.getElementById('score-desk');
-	desk.style.display = 'block';
-}
-
-function onemoreGame(){
-	var onemore = document.getElementById('onemore');
-	onemore.style.display = 'block';
-}
-
-function mainMenu(){
 	var onemore = document.getElementById('onemore');
 	onemore.style.display = 'none';
 
@@ -72,20 +12,87 @@ function mainMenu(){
 	var desk = document.getElementById('score-desk');
 	desk.style.display = 'none';
 
+	var panel = document.getElementById('partner-panel');
+	panel.style.display = 'block';
+
+	var prevpage = document.getElementById('to-prev-page');
+	prevpage.style.display = 'block';
+
+	currentPage = 'single';
+}
+/*Uveli4ivaet izobrajenie pri navedenii kursora */
+function growUpImage(defId, imgId){	
+	var definition = document.getElementById(defId);
+	definition.style.color = 'blue'
+	definition.style.fontSize = '35';
+
+	var image  =  document.getElementById(imgId);
+	image.style.height = '90%';
+	image.style.width = '90%';
+}
+/*Umenwaet izobrajenie pri navedenii kursora */
+function growDownImage(defId, imgId){	
+	var definition = document.getElementById(defId);
+	definition.style.color = 'white'
+	definition.style.fontSize = '30';	
+
+	var image  =  document.getElementById(imgId);
+	image.style.height = '80%';
+	image.style.width = '80%';
+}
+/*na4alo igri s 4elovekom*/
+function startWithHuman(){
+	var panel = document.getElementById('partner-panel');
+	panel.style.display = 'none';
+
+	var content = document.getElementById('tic-tac-wrap');
+	content.style.display = 'block';
+
+	var desk = document.getElementById('score-desk');
+	desk.style.display = 'block';
+
+	currentPage = 'game';
+}
+/*Posleigrovoe menu */ 
+function onemoreGame(){
+	var onemore = document.getElementById('onemore');
+	onemore.style.display = 'block';
+}
+/*V glavnoe menu*/ 
+function mainMenu(){
+	var onemore = document.getElementById('onemore');
+	onemore.style.display = 'none';
+
+	var content = document.getElementById('tic-tac-wrap');
+	content.style.display = 'none';
+
+	var panel = document.getElementById('partner-panel');
+	panel.style.display = 'none';
+
+	var desk = document.getElementById('score-desk');
+	desk.style.display = 'none';
+
+	var prevpage = document.getElementById('to-prev-page');
+	prevpage.style.display = 'none';
+
 	var content = document.getElementById('menu-bar');
 	content.style.display = 'block';
 
+	currentPage = 'main';
+
 	clean(initStep, initMaxStep);
 }
-
+/*Sleduywaya partiya*/
 function nextRound(){
 	var onemore = document.getElementById('onemore');
 	onemore.style.display = 'none';
 
+	currentPage = 'game';
+
 	currentRound++;
 	clean(currentRound, currentRound + initMaxStep);
 }
-
+/*4istit pole ot kamnei i listo4kov*/
 function clean(curStep, mStep){
 	for(var id = 0; id < initMaxStep; id++){
 		var d = document.getElementById(id+1);
@@ -94,4 +101,34 @@ function clean(curStep, mStep){
 	currentStep = curStep;
 	maxStep = mStep;
 	gameover = false;
+}
+/*K prediduwei stranice */
+function backToPrevPage(){
+	if('game' === String(currentPage)){
+		singleGame();
+		clean(initStep, initMaxStep);
+	}
+	else if('single' === String(currentPage)){
+		mainMenu();
+	}
+}
+/*PUUUUUUUUUSH*/
+function push(elem){
+	if(!gameover){
+		var d = document.getElementById(elem);
+		if(d.innerHTML == ''){
+			if(currentStep % 2 == 0){
+				d.innerHTML = '<div class="round">';
+			}
+			else{
+				d.innerHTML = '<div class="cross">';
+			}
+			currentStep++;
+		}
+		if(currentStep == maxStep){
+			alert('Draw!!!');
+			gameover = true;
+			onemoreGame();
+		}
+	}
 }
